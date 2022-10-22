7f1636b6144de1c3cd128f336db8a8a18e1a5abb
SijinNalpadi <116311203+SijinNalpadi@users.noreply.github.com>
2022-10-22 18:51:32 +0100

    Update and rename test.sh to apptest.sh

---
+++ apptest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://54.226.103.56/greeting)
+
+if [[ "$status_code" -ne 200 ]] ; then
+  echo "API Health Check Failed..." 
+  exit 1
+  
+else
+  echo "API Health Check Success..."
+  content=$(curl -s http://54.226.103.56:32337/greeting|jq .content|tr -d '"'|cut -d ',' -f1)
+  if [[ "$content" == "Hello" ]] ; then
+    echo "API Functionality is Success..."
+    exit 0
+  else
+    echo "API Functionality is Failed..."
+    exit 1
+  fi	
+	
+fi
