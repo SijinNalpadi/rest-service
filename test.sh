d7931b6f7d2233fa6354da13e4b40f58078a496b
SijinNalpadi <116311203+SijinNalpadi@users.noreply.github.com>
2022-10-22 17:49:47 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,17 +5,17 @@ status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://5
 if [[ "$status_code" -ne 200 ]] ; then
   echo "Health check failed" 
   exit 1
-  echo "FAILED" > /tmp/status
+  echo "FAILED" > commandResult
 else
   echo "Health check sucess"
   content=$(curl -s http://54.226.103.56:32337/greeting|jq .content|tr -d '"'|cut -d ',' -f1)
   if [[ "$content" == "Hello" ]] ; then
     echo "API is working"
 	exit 0 
-	echo "FAILED" > /tmp/status
+	echo "FAILED" > commandResult
   else
 	exit 1
-	echo "FAILED" > /tmp/status
+	echo "FAILED" > commandResult
   fi	
 	
 fi
