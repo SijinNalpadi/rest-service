cf27c4a8bc5ff62605b5c46bc8adeb4d2afe7249
SijinNalpadi <116311203+SijinNalpadi@users.noreply.github.com>
2022-10-23 13:12:55 +0100

    Update apptest.sh

--- apptest.sh
+++ apptest.sh
@@ -2,7 +2,7 @@
 
 echo "Waiting For API to be Ready..."
 sleep 30
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://54.226.103.56/greeting)
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://52.87.174.4/greeting)
 
 if [[ "$status_code" -ne 200 ]] ; then
   echo "API Health Check Failed..." 
@@ -10,7 +10,7 @@ if [[ "$status_code" -ne 200 ]] ; then
   
 else
   echo "API Health Check Success..."
-  content=$(curl -s http://54.226.103.56/greeting|jq .content|tr -d '"'|cut -d ',' -f1)
+  content=$(curl -s http://52.87.174.4/greeting|jq .content|tr -d '"'|cut -d ',' -f1)
   if [[ "$content" == "Hello" ]] ; then
     echo "API Functionality Check Success..."
     exit 0
